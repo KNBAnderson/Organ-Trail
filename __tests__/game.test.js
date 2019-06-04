9e9f227967304c96ce2cbd6bed3b95f0b1d66dd8
dustin herboldshimer and katlin anderson <dustin+katlin@>
2019-06-04 16:17:36 -0700

    test dayEnd and end game  methods in Game class

---
+++ __tests__/game.test.js
@@ -0,0 +1,53 @@
+import { Game } from './../src/game';
+import { Organ } from '../src/organ';
+
+describe('game', () => {
+  let testGame;
+
+  beforeEach(()=>{
+    testGame = new Game();
+  });
+
+  describe('Game constructor', () => {
+    test('should instantiate new Game object', () => {
+      expect(typeof testGame).toBe('object');
+      expect(typeof testGame.party).toBe('object');
+      expect(testGame.locations.length).toEqual(6);
+      expect(testGame.events.length).toEqual(6);
+      expect(testGame.day).toEqual(0);
+    });
+  });
+
+  describe('game methods', () => {
+    test('should check if game is over', () => {
+      expect(testGame.isGameOver()).toBe(false);
+      let loseGame = new Game();
+      loseGame.party.donors.forEach(donor => {
+        donor.health = 0;
+      });
+      expect(loseGame.isGameOver()).toEqual('Lose');
+      let winGame = new Game();
+      winGame.party.distanceTraveled = 2000;
+      expect(winGame.isGameOver()).toEqual('Win');
+    })
+
+    test('should check that the game does things at the end of the day', () => {
+      testGame.party.iceChest.organInv.push(new Organ('test', 3, 3, 5));
+      testGame.party.iceChest.organInv.push(new Organ('other organ', 34, 23, 12));
+      testGame.dayEnd();
+      expect(testGame.party.iceChest.organInv.length).toEqual(1);
+      expect(testGame.party.donors[2].health).toEqual(95);
+      testGame.party.inventory['ice'] = 5;
+      testGame.dayEnd();
+      expect(testGame.party.inventory['ice']).toEqual(4);
+      
+      testGame.party.distanceTraveled = 3000;
+      expect(testGame.dayEnd()).toEqual(true);
+      testGame.party.distanceTraveled = 4;
+      testGame.party.donors.forEach(donor => {
+        donor.health = 0;
+      });
+      expect(testGame.dayEnd()).toBe(false);
+    })
+  })
+})
--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import { Party } from './party';
 import { initLocations } from './location';
 import { Event, initEvents } from './event';
+import {Donor } from './../src/donor';
 
 export class Game {
   constructor() {
@@ -15,10 +16,11 @@ export class Game {
   //   if (this.party.distanceTraveled > this.locations[0].distance) {
 
   //   }
-  }
+  //}
+  
 
-  isGameOver () {
-    if (this.party.donors.every(donor => {donor.health <= 0;})) {
+  isGameOver() {
+    if (this.party.donors.every(checkDead)) {
       return 'Lose';
     } else if (this.party.distanceTraveled > 1500) {
       return 'Win';
@@ -27,45 +29,56 @@ export class Game {
     }
   }
 
+  gameWon() {
+    return true;
+  }
+
+  gameLost() {
+    return false;
+  }
+
   dayEnd() {
     if (!this.isGameOver()) {
     //check ice
-    if(this.party.invetory.ice < 1 && this.party.iceChest.organInv.length > 1) {
-      this.party.iceChest.organInv.pop();
-      this.party.invetory.ice = 0;
-    } else {
-      this.party.invetory.ice--;
-    }
-    //check first aid level
-    if (this.party.inventory.firstaid < 1) {
-      this.party.donors.forEach(donor => {
-        if (!donor.isDead()) {
-          donor.health -= 5;
-        }
-      })
-    }
-    this.party.updateSpeed();
-    this.party.updateDistanceTraveled();
-    let random = Math.floor(Math.random() * (25 + 1 - 0)) + 0; 
-    if (random % 3 === 0) {
-      let event = randomize(this.events);
-     // event.launchEventdsiplay();
-    }
-    this.day++;
-    this.checkLocation();
+      if(this.party.inventory['ice'] < 1 && this.party.iceChest.organInv.length > 1) {
+        this.party.iceChest.organInv.pop();
+        this.party.inventory.ice = 0;
+      } else {
+        this.party.inventory.ice--;
+      }
+      //check first aid level
+      if (this.party.inventory['firstaid'] < 1) {
+        this.party.donors.forEach(donor => {
+          if (!donor.isDead()) {
+            donor.health -= 5;
+          }
+        })
+      }
+      this.party.updateSpeed();
+      this.party.updateDistanceTraveled();
+      let random = Math.floor(Math.random() * (25 + 1 - 0)) + 0; 
+      if (random % 3 === 0) {
+        let event = randomize(this.events);
+        // event.launchEventdsiplay();
+      }
+      this.day++;
+    //this.checkLocation();
     } else if (this.isGameOver() === 'Win'){
-      //stuff if game won
+      console.log('here');
+      return this.gameWon();
     } else if (this.isGameOver() === 'Lose') {
-      //stuff if game lost
-    } else {
-      console.log('Error with endGame function at dayEnd')
-    }
+      return this.gameLost(); 
+    } 
   } 
 }
+
 export function randomize(arr) {
-  let max = arr.length;
+  let max = arr.length - 1;
   let min = 0;
   let random = Math.floor(Math.random() * (max + 1 - min)) + min;
   return arr[random];
 }
 
+function checkDead(element) {
+  return element.health < 1;
+} 
