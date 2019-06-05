fd0dcfbfa89d6f148ee731504f15925037c7b412
Katlin <katlin.nbanderson@gmail.com>
2019-06-04 21:49:00 -0700

    refactor game tests

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -3,9 +3,17 @@ import { Organ } from '../src/organ';
 
 describe('game', () => {
   let testGame;
+  let loseGame;
+  let winGame;
 
   beforeEach(()=>{
     testGame = new Game();
+    loseGame = new Game();
+    loseGame.party.donors.forEach(donor => {
+      donor.health = 0;
+    });
+    winGame = new Game();
+    winGame.party.distanceTraveled = 2000;
   });
 
   describe('Game constructor', () => {
@@ -21,13 +29,7 @@ describe('game', () => {
   describe('game methods', () => {
     test('should check if game is over', () => {
       expect(testGame.isGameOver()).toBe(false);
-      let loseGame = new Game();
-      loseGame.party.donors.forEach(donor => {
-        donor.health = 0;
-      });
       expect(loseGame.isGameOver()).toEqual('Lose');
-      let winGame = new Game();
-      winGame.party.distanceTraveled = 2000;
       expect(winGame.isGameOver()).toEqual('Win');
     })
 
@@ -40,19 +42,14 @@ describe('game', () => {
       testGame.party.inventory['ice'] = 5;
       testGame.dayEnd();
       expect(testGame.party.inventory['ice']).toEqual(4);
-      testGame.party.distanceTraveled = 3000;
-      expect(testGame.dayEnd()).toEqual(true);
-      testGame.party.distanceTraveled = 4;
-      testGame.party.donors.forEach(donor => {
-        donor.health = 0;
-      });
-      expect(testGame.dayEnd()).toBe(false);
+      expect(winGame.dayEnd()).toEqual(true);
+      expect(loseGame.dayEnd()).toBe(false);
     })
 
     test('should return random element from an array', () => {
       let array = [7, 8, 9, 10];
       expect(typeof randomize(array)).toBe('number');
-      expect(randomize(array)).toBeGreaterThan(7);
+      expect(randomize(array)).toBeGreaterThan(6);
       expect(randomize(array)).toBeLessThan(11);
     })
   })
